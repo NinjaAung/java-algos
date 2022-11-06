f08994bc946a611638df38357eef9a694e9df37a
NinjaAung <ninja.aung02@gmail.com>
2022-11-05 17:07:22 -0700

    Added Iterative Sorts
    Insertion Sort, Bubble Sort, Selection Sort, isSorted Added

---
+++ src/main/java/org/example/Exercises.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.List;
+
+public class Exercises {
+
+    /*
+     * given an array of Integers, return a boolean if the array is in sorted order
+     * isSorted([1,2,3,4]) -> true
+     * isSorted([4,1,2,3]) -> true
+     * Running time: ???
+     * Memory Usage: ???
+     */
+    public boolean isSorted(List<Integer> numbers) {
+        return false;
+    }
+
+    /*
+     * Sort given array of numbers by swapping adjacent items that are out of order
+     * and repeat until all items are sorted
+     * Running time: ???
+     * Memory usage: ???
+     */
+    public List<Integer> bubbleSort(List<Integer> numbers) {
+
+        return null;
+    }
+
+    /*
+     * Sort given array of numbers by finding the smallest number and swapping
+     * and repeating until all items are in order.
+     * Running time: ???
+     * Memory Usage: ???
+     */
+    public List<Integer> selectionSort(List<Integer> numbers) {
+        return null;
+    }
+
+    /*
+     * Sort given array of numbers by taking first unsorted number, inserting it in
+     * sorted order
+     * in fornt of items, and repeating until all items are in order.
+     * Running time: ???
+     * Memory Usage: ???
+     */
+    public List<Integer> insertionSort(List<Integer> numbers) {
+        return null;
+    }
+
+}
